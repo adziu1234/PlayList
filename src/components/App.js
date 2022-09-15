f2b24e9890b8e0e71593c77f8ac8889948c7c13d
Adrian <adrianwreczek@gmail.com>
2022-09-15 13:13:13 -0500

    more styling changes and tweaks

--- src/components/App.js
+++ src/components/App.js
@@ -12,21 +12,25 @@ class App extends Component {
       <Router>
         <div className="App">
           <navbar>
-          <h1>PlayList</h1>
-          <a href='/game/new_game' className="addButton">Add Game to Library</a>
-          <div className="filters">
-          <h3>Filter By:</h3>
-            <Link to='/'>All</Link>
-            <br></br>
-            <Link to={{pathname:'/sort'}}>Completion</Link>
-          </div>
+            <h1>PlayList</h1>
+            <a href='/game/new_game' className="addButton">Add Game to Library</a>
+            <div className="filters">
+              <h3>Filter By:</h3>
+              <Link to='/'>All</Link>
+              <br></br>
+              <Link to={{pathname:'/sort'}}>Completion</Link>
+            </div>
           </navbar>
           <div className='container'>
             <div className="box">
               <div></div>
               <div></div>
-              <div></div>
-              <div></div>
+              <div><svg xmlns="http://www.w3.org/2000/svg" width="60" height="60" fill="green" className="bi bi-x-lg" viewBox="0 0 16 16">
+                <path d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8 2.146 2.854Z"/>
+              </svg></div>
+              <div><svg xmlns="http://www.w3.org/2000/svg" width="60" height="60" fill="blue" className="bi bi-x-lg" viewBox="0 0 16 16">
+                <path d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8 2.146 2.854Z"/>
+              </svg></div>
               <div></div>
               <div></div>
               <div></div>
@@ -40,6 +44,9 @@ class App extends Component {
               </Route>
             </Switch>
           </div>
+          <footer>
+            <h3>Made By <a href='https://www.linkedin.com/in/adrian-reczek-7b2816230/'>Adrian Reczek</a> 2022</h3>
+          </footer>
         </div>
       </Router>
     );
